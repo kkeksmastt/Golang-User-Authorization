050cbdcdf99de2b6efb14a1cad9674f2ed5a7037
Anvar <71346491+kkeksmastt@users.noreply.github.com>
2024-03-02 17:55:09 +0300

    Update serverLog.go

--- UserAuth/serverLog/serverLog.go
+++ UserAuth/serverLog/serverLog.go
@@ -1,19 +1,18 @@
 package serverlog
 
 import (
-	c "UserAuth/color"
 	"log"
 	"os"
 )
 
 func Info(str string) {
-	log.New(os.Stdout, c.Green+"INFO\t"+c.Reset, log.Ldate|log.Ltime).Printf(str)
+	log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime).Printf(str)
 }
 
 func ErrorLog(err error) {
-	log.New(os.Stderr, c.Red+"ERROR\t"+c.Reset, log.Ldate|log.Ltime|log.Lshortfile).Println(err)
+	log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile).Println(err)
 }
 
 func ErrorFatal(err error) {
-	log.New(os.Stderr, c.Red+"ERROR\t"+c.Reset, log.Ldate|log.Ltime|log.Lshortfile).Fatal(err)
+	log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile).Fatal(err)
 }
